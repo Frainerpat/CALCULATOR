5bca09132ca84b4df5cc9fabd665a242d4f9d0ed
Frainerpat <Frainerpat@gmail.com>
2024-09-01 21:45:49 +0300

    first commit

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,31 @@
+﻿namespace ClassLibrary1;
+
+public class Class1
+{
+    static void Main(string[] arg)
+    {
+        Console.WriteLine("Добро пожаловать в калькулятор");
+        Console.WriteLine("Введите первое число");
+        int pervoechislo = int.Parse(Console.ReadLine());
+        Console.WriteLine("Введите *, /, + или -");
+        string oprachia = Console.ReadLine();
+        Console.WriteLine("Введите 2 число");
+        int vtoroechislo = int.Parse(Console.ReadLine());
+        if (oprachia == "+")
+        {
+            Console.WriteLine(pervoechislo + vtoroechislo);
+        }
+        if (oprachia == "-")
+        {
+            Console.WriteLine(pervoechislo - vtoroechislo);
+        }
+        if (oprachia == "*")
+        {
+            Console.WriteLine(pervoechislo * vtoroechislo);
+        }
+        if (oprachia == "/")
+        {
+            Console.WriteLine(pervoechislo / vtoroechislo);
+        }
+    }
+}
